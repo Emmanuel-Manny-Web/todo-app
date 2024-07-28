57bdad29e19be84b4116a8366a6484d995f173d4
Emmanuel-Manny-Web <ipaliboha@gmail.com>
2024-07-28 18:18:52 +0100

    save

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,16 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <KindeProvider
       clientId="8efc2e0acefd4a848cb34c75420c6a18"
       domain="https://todoreactapp.kinde.com"
-      redirectUri="http://localhost:5173"
-      logoutUri="http://localhost:5173"
+      redirectUri={
+        process.env.NODE_ENV === "production"
+          ? "https://todo-app-black-omega.vercel.app"
+          : "http://localhost:5173"
+      }
+      logoutUri={
+        process.env.NODE_ENV === "production"
+          ? "https://todo-app-black-omega.vercel.app"
+          : "http://localhost:5173"
+      }
     >
       <UserProvider>
         <App />
